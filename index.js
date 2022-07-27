efd4880a70760abdebc17efeab995670acf96844
ItaiMordekovich <morduch8@gmail.com>
2022-07-27 23:02:00 +0300

    user routes, user controllers, auth middleware

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
-import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import cors from "cors";
-import dotenv from 'dotenv';
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
 import postRoutes from './routes/posts.js';
+import userRouter from "./routes/user.js";
 
 const app = express();
-dotenv.config();
 
-
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
+app.use("/user", userRouter);
 
-app.get('/', (req, res) => {
-    res.send('Hello to Travel API')
-})
-
-const CONNECTION_URL = process.env.CONNECTION_URL;
-const PORT = process.env.PORT || 5000;
+const CONNECTION_URL = 'mongodb+srv://morduch8:dwarfdwarf1@cluster0.yufc5.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-.then (() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-.catch ((error) => console.log(error.message));
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set('useFindAndModify', false);
